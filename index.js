fb79772ce53b2dab524fe447b52b060bf4775dc8
Arun Kumar <aruncode49@gmail.com>
2023-09-05 23:17:40 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// static middleware
+app.use(express.static("public"));
+
+// Db password: eBEZ4o4Mo0WjHH67
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURL =
+  "mongodb+srv://aruncode49:eBEZ4o4Mo0WjHH67@cluster0.l3qygh1.mongodb.net/node-auth";
+mongoose
+  .connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log("MongoDB Connected");
+    app.listen(3000, () => console.log("Server is running at 3000"));
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
