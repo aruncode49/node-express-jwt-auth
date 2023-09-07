9ce119c80c87df49699d7efcadf9176010654c94
Arun Kumar <aruncode49@gmail.com>
2023-09-07 12:54:42 +0530

    Login Part-1

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,8 +50,12 @@ async function signup_post(req, res) {
 
 async function login_post(req, res) {
   const { email, password } = req.body;
-  console.log(email, password);
-  res.send("user login");
+  try {
+    const user = await User.login(email, password);
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(400).json({});
+  }
 }
 
 module.exports = {
--- models/user.js
+++ models/user.js
@@ -24,6 +24,19 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+// Static method to login user
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("incorrect password");
+  }
+  throw Error("incorrect email");
+};
+
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
